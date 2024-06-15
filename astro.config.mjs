f205a14a84df1f517ffc86d2b373ae36bc5bffb5
Shaked Hagag <shaked.hagag@openweb.com>
2024-06-15 12:09:46 +0300

    packages for mdx, katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx"; // https://astro.build/config
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  markdown: {
+    rehypePlugins: [rehypeHeadingIds, rehypeAccessibleEmojis, rehypeKatex],
+    remarkPlugins: [remarkToc, remarkMath],
+  },
 });
+
