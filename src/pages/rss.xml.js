1b97630cf573b2322fcc3f2a5655cd18ed9b39d6
Shaked Hagag <shaked.hagag@openweb.com>
2024-06-15 12:09:58 +0300

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,17 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = Object.values(postImportResult);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.frontmatter.title,
+      description: post.frontmatter.spoiler,
+      link: post.frontmatter.href,
+      pubDate: new Date(post.frontmatter.date),
+    })),
+  });
 }
