3eace9a0bd890d4b4e8199469a16132e18247e61
shakedhagag <shakedc200@gmail.com>
2025-03-07 13:49:59 +0200

    adjust reading time to code blocks

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,17 @@ export function getPostWords(content: string) {
   return content.split(" ").filter(Boolean).length;
 }
 
-const WORDS_PER_MINUTE = 200;
-export function readingTime(wordsCount: number) {
-  return Math.ceil(wordsCount / WORDS_PER_MINUTE);
+export function readingTime(content: string | any) {
+  // Convert to string or use empty string if not valid
+  const contentStr = typeof content === "string" ? content : "";
+
+  const wordsPerMinute = 200;
+  const codeBlockMatches = contentStr.match(/```[\s\S]*?```/g) || [];
+
+  const codeBlockWords = codeBlockMatches.join(" ").split(/\s+/).length;
+  const normalWords = contentStr
+    .replace(/```[\s\S]*?```/g, "")
+    .split(/\s+/).length;
+
+  return Math.ceil(normalWords / wordsPerMinute + codeBlockWords / 100);
 }
