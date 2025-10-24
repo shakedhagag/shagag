8e28278b83d8eec2e86edd8f2be9c0e6a198c86b
Shaked Hagag <shaked@frame.security>
2025-10-24 16:28:35 +0300

    fix blog structure after bumping astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,17 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
+import { group } from "node:console";
 
 const blogCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    spoiler: z.string(),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		date: z.string(),
+		spoiler: z.string(),
+		group: z.string().optional(),
+		customUrl: z.string().optional(),
+	}),
 });
 
 export const collections = {
-  posts: blogCollection,
+	posts: blogCollection,
 };
