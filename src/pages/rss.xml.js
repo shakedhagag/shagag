410ac5920a483fdfa7e6beca0f819083af4f4589
Shaked Hagag <shaked@frame.security>
2025-10-24 16:46:59 +0300

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,22 @@
+import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-  const postImportResult = import.meta.glob("../content/posts/**/*.mdx", {
-    eager: true,
-  });
-  const posts = Object.values(postImportResult);
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    items: posts.map((post) => ({
-      title: post.frontmatter.title,
-      description: post.frontmatter.spoiler,
-      link: post.frontmatter.href,
-      pubDate: new Date(post.frontmatter.date),
-    })),
-  });
+	const posts = await getCollection("posts");
+
+	// Filter out posts with groups if you only want main posts in RSS
+	const mainPosts = posts.filter((post) => !post.data.group);
+
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: mainPosts.map((post) => ({
+			title: post.data.title,
+			description: post.data.spoiler,
+			link: post.data.customUrl || `/${post.slug}/`, // The docs show using the slug directly
+			pubDate: new Date(post.data.date),
+		})),
+	});
 }
